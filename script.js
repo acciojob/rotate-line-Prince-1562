9d9d2c66881dbc6b366b5b13219cb5bc05657fee
Prince-1562 <princemakani1562@gmail.com>
2025-05-31 08:40:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+// script.js
+const line = document.getElementById('line');
+let angle = 0;
+const rotationSpeed = 2; // Degrees to rotate each interval
+const intervalTime = 20; // Milliseconds between rotations
+
+function rotateLine() {
+    angle += rotationSpeed;
+    line.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`;
+}
+
+setInterval(rotateLine, intervalTime);
